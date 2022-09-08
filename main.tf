d3113e1cde182529852a2d9a262f01a11234028a
balarajumanojkumar <manojkumar.balaraju@gmail.com>
2022-09-08 13:19:18 +0530

    Added aws instance in main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+resource "aws_instance" "manojinstance" {
+    ami            = "ami-04ff9e9b51c1f62ca"
+    instance_type  = "t2.micro"
+    vpc_security_group_ids =[ "sg-03919f41268cdfb36"]
+    tags           = {
+        Name       = "ec2-instance"
+    }
+}
