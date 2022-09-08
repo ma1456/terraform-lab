73a280b51a39e844b0ca83d6d375f3bd3a74c7fc
balarajumanojkumar <manojkumar.balaraju@gmail.com>
2022-09-08 14:43:20 +0530

    Added changes

--- Provider.tf
+++ Provider.tf
@@ -1,4 +1,6 @@
 provider "aws" {
-    region = var.region
-  
+    access_key = "AKIA5OH7VEFYB24N5KOW"
+  secret_key = "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"
+  region     = "ap-southeast-1"
+    
 }
--- variable.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-    type     = string
-    default  = "ap-southeast-1"
-  
-}
