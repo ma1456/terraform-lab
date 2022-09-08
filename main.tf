c095fde5b5f96fc3ad40918b6dcd0509e1719621
balarajumanojkumar <manojkumar.balaraju@gmail.com>
2022-09-08 13:46:13 +0530

    Added changes in main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "b" {
 resource "aws_instance" "manojinstance" {
     ami            = "ami-04ff9e9b51c1f62ca"
     instance_type  = "t2.micro"
-    vpc_security_group_ids =[ "sg-03919f41268cdfb36"]
+    vpc_security_group_ids =[ "sg-02073c41eba50b4e2"]
     tags           = {
         Name       = "ec2-instance"
     }
