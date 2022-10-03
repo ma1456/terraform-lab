1618ac0a1083b568820f09518bcf5e31d8b2038a
balarajumanojkumar <manojkumar.balaraju@gmail.com>
2022-10-03 09:37:49 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_instance" "jenkins" {
+resource "aws_instance" "my instance" {
     ami            = "ami-00e912d13fbb4f225"
     instance_type  = "t2.micro"
-    vpc_security_group_ids =[ "sg-0d9a7a5aa16aeefb9"]
+    vpc_security_group_ids =["sg-0d9a7a5aa16aeefb9"]
     tags           = {
         Name       = "ec2-instance"
     }
