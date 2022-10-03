3a14c203522522a5eaa48e56cd93e4015a569a64
balarajumanojkumar <manojkumar.balaraju@gmail.com>
2022-10-03 09:39:45 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_instance" "my instance" {
+resource "aws_instance" "myinstance" {
     ami            = "ami-00e912d13fbb4f225"
     instance_type  = "t2.micro"
     vpc_security_group_ids =["sg-0d9a7a5aa16aeefb9"]
