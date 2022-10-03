4463aa9dbbb21b3cdcc4db8a5cbc1f58ab3d5868
balarajumanojkumar <manojkumar.balaraju@gmail.com>
2022-10-03 10:04:32 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -4,10 +4,9 @@ resource "aws_s3_bucket" "b" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-  }
-}
 
-resource "aws_instance" "web" {
+
+resource "aws_instance" "web"{
     ami            = "ami-00e912d13fbb4f225"
     instance_type  = "t2.micro"
     vpc_security_group_ids =["sg-0d9a7a5aa16aeefb9"]
